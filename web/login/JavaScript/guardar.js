2ba24d4aae76ca384c51137c8830bcce2fedf3fa
Tomás Gutiérrez Orozco <tomasgo2004@gmail.com>
2023-11-08 21:37:54 -0500

    Se corrigen errores

--- web/login/JavaScript/guardar.js
+++ web/login/JavaScript/guardar.js
@@ -1,21 +1,32 @@
 // Código para ejecutar la función `guardar()`
 document.getElementById("ingresar").addEventListener("click", function () {
     // Obtenemos los valores del correo y contraseña
-    const correo = document.getElementById("correo").value;
+    const correo = document.getElementById("correo").value.trim();
     const contraseña = document.getElementById("contraseña").value;
+
+    // Validar que el correo no esté vacío y tenga formato de correo electrónico
+    if (!validarCorreo(correo)) {
+        mostrarMensaje("Por favor, ingresa un correo electrónico válido");
+        return;
+    }
+
+    // Validar que la contraseña no esté vacía
+    if (contraseña.trim() === '') {
+        mostrarMensaje("Por favor, ingresa una contraseña");
+        return;
+    }
 
     // Consultar si el usuario ya existe en Firestore
     db.collection("usuarios").where("usuario", "==", correo)
         .get()
         .then((querySnapshot) => {
             if (querySnapshot.empty) {
-                // El usuario no existe, guardamos los datos y mostramos mensaje
+                // El usuario no existe, guardamos los datos y redirigimos
                 db.collection("usuarios").add({
                     usuario: correo,
                     contraseña: contraseña,
                 }).then(() => {
-                    // Redirigir a otra página (puedes agregar el código aquí)
-                    mostrarMensaje("Usuario registrado correctamente");
+                    // Redirigir a otra página
                     redirigir('/web/PaginaPrincipal/index.html');
                 });
             } else {
@@ -23,12 +34,11 @@ document.getElementById("ingresar").addEventListener("click", function () {
                 querySnapshot.forEach((doc) => {
                     const usuarioData = doc.data();
                     if (usuarioData.contraseña === contraseña) {
-                        // Contraseña correcta, redirigir a otra página (puedes agregar el código aquí)
-                        mostrarMensaje("Inicio de sesión exitoso");
+                        // Contraseña correcta, redirigir a otra página
                         redirigir('/web/PaginaPrincipal/index.html');
                     } else {
                         // Contraseña incorrecta, mostrar mensaje de error
-                        mostrarMensaje2("Contraseña incorrecta");
+                        mostrarMensaje("Contraseña incorrecta");
                     }
                 });
             }
@@ -38,29 +48,24 @@ document.getElementById("ingresar").addEventListener("click", function () {
         });
 });
 
+// Función para validar formato de correo electrónico con expresión regular
+function validarCorreo(correo) {
+    const expresionRegularCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return expresionRegularCorreo.test(correo);
+}
+
 // Función para mostrar mensajes con SweetAlert2
 function mostrarMensaje(mensaje) {
     Swal.fire({
         position: 'top-end',
-        icon: 'success', // Puedes cambiar 'success' por 'error' para mensajes de error
+        icon: 'error',
         title: mensaje,
         showConfirmButton: false,
         timer: 1500
     });
 }
 
-// Función para mostrar mensajes con SweetAlert2
-function mostrarMensaje2(mensaje) {
-    Swal.fire({
-        position: 'top-end',
-        icon: 'error', // Puedes cambiar 'success' por 'error' para mensajes de error
-        title: mensaje,
-        showConfirmButton: false,
-        timer: 1500
-    });
-}
-
-
+// Función para redirigir a otra página
 function redirigir(pagina) {
     window.location.href = pagina;
 }
